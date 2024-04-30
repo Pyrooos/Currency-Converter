e2f684c00a1a4edea79844e2c887769408b86c26
Cris <cristian_guher@outlook.com>
2024-04-29 19:30:43 -0600

    se reduce texto en switch case y se reduce codigo

--- CurrencyConverter/src/Main.java
+++ CurrencyConverter/src/Main.java
@@ -46,20 +46,6 @@ public class Main {
                 break;
         }
     }
-    private static String[] getCurrencySelection(int option, List<String> menuCurrency, Scanner keyboard) {
-        String[] currencies = new String[2];
-        String selectedCurrency = menuCurrency.get(option - 1);
-        String[] parts = selectedCurrency.split(" ");
-        currencies[0] = parts[1];
-        System.out.println("You selected: " + currencies[0] + "\n");
-        System.out.println("Please enter the number option for the second currency:");
-        int optionCurrency2 = keyboard.nextInt();
-        String selectedCurrency2 = menuCurrency.get(optionCurrency2 - 1);
-        String[] parts2 = selectedCurrency2.split(" ");
-        currencies[1] = parts2[1];
-        System.out.println("You selected: " + currencies[1] + "\n");
-        return currencies;
-    }
 
     private static void displayMenu(List<String> menuCurrency) {
         System.out.println("*************************************\n\n             Menu\n");
